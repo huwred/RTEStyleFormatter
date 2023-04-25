95bf969142fe1db10482d984cb3bb4ee17c35ec7
huwred <huw@forum.snitz.com>
2023-04-25 15:04:09 +0100

    Add project files.

---
+++ AppSettingsUpdater.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Hosting;
+
+namespace RTEStyleFormatter
+{
+    /// <summary>
+    /// Utility class to update/create values in appsettings.json file
+    /// </summary>
+    public class AppSettingsUpdater
+    {
+        private const string emptyJson = "{}";
+        private const string settinsgFileName = "appsettings.json";
+        private readonly IWebHostEnvironment _env;
+
+        public AppSettingsUpdater(IWebHostEnvironment webHostEnvironment)
+        {
+            _env = webHostEnvironment ?? throw new ArgumentNullException(nameof(webHostEnvironment));
+        }
+        /// <summary>
+        /// This method will update the value for a given key in the json file
+        /// </summary>
+        /// <param name="key">Name of the json key</param>
+        /// <param name="value">New value for the setting</param>
+        /// <exception cref="ArgumentException"></exception>
+        public void UpdateAppSetting(string key, object value)
+        {
+            if (key == null)
+            {
+                throw new ArgumentException("Json property key cannot be null", nameof(key));
+            }
+            
+            var config = File.ReadAllText(settinsgFileName);
+            
+            var updatedConfigDict = UpdateJson(key, value, config);
+            // After receiving the dictionary with updated key value pair, we serialize it back into json.
+            var updatedJson = JsonSerializer.Serialize(updatedConfigDict, new JsonSerializerOptions { WriteIndented = true });
+
+            File.WriteAllText(settinsgFileName, updatedJson,Encoding.UTF8);
+        }
+
+
+        /// <summary>
+        /// This method will recursively read json segments separated by semicolon (firstObject:nestedObject:someProperty)
+        /// until it reaches the desired property that needs to be updated,
+        /// it will update the property and return json document represented by dictonary of dictionaries of dictionaries and so on.
+        /// This dictionary structure can be easily serialized back into json
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="jsonSegment"></param>
+        /// <returns></returns>
+        private Dictionary<string, object> UpdateJson(string key, object value, string jsonSegment)
+        {
+            const char keySeparator = ':';
+
+            var config = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonSegment);
+            if (config != null)
+            {
+                var keyParts = key.Split(keySeparator);
+                if (keyParts.Length > 1)
+                {
+                    var firstKeyPart = keyParts[0];
+                    var remainingKey = string.Join(keySeparator, keyParts.Skip(1));
+
+                    // If the key does not exist already, we will create a new key and append it to the json
+                    var newJsonSegment = config.ContainsKey(firstKeyPart) && config[firstKeyPart] != null
+                        ? config[firstKeyPart].ToString()
+                        : emptyJson;
+                    config[firstKeyPart] = UpdateJson(remainingKey, value, newJsonSegment);
+                }
+                else
+                {
+                    config[key] = value;
+                }
+            }
+
+            return config;
+        }
+
+    }
+}
---
+++ LoadTinyMCEConfig.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+
+namespace RTEStyleFormatter
+{
+    public class LoadTinyMCEConfig : IHostedService
+    {
+        private readonly IWebHostEnvironment _env;
+        public LoadTinyMCEConfig(IWebHostEnvironment webHostEnvironment)
+        {
+            _env = webHostEnvironment ?? throw new ArgumentNullException(nameof(webHostEnvironment));
+        }
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            string settingsFile = "App_Plugins/TinyMCEStyleFormatter/RTE_style_formats.json";
+            //does the json file exist?
+            //if yes, minify it, escape the " and set the value in appsettings.json
+            var styleSettings = _env.ContentRootFileProvider.GetFileInfo(settingsFile);
+            if (styleSettings.Exists)
+            {
+                var fs = styleSettings.CreateReadStream();
+                long _TotalBytes = fs.Length;
+
+                // attach filestream to binary reader
+                // convert stream to string
+                StreamReader reader = new StreamReader( fs );
+                string text = reader.ReadToEnd();
+
+                //read the json from the file
+                object obj = JsonConvert.DeserializeObject(text);
+                //serialize to a minified jsonstring
+                var jsonString = JsonConvert.SerializeObject(obj, Formatting.None);
+
+                new AppSettingsUpdater(_env).UpdateAppSetting("Umbraco:CMS:RichTextEditor:CustomConfig:style_formats", jsonString);
+
+            }
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
